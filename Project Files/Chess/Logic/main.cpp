7fc51a4fe57d54950c9c7ddd46d9e5889f7a73f1
Oliver Lindgren <oliver.lindgren@outlook.com>
2021-04-28 00:50:19 +0200

    Timing

--- Project Files/Chess/Logic/main.cpp
+++ Project Files/Chess/Logic/main.cpp
@@ -1,12 +1,15 @@
 #include"Game.hpp"
+#include<time.h>
 
 using namespace std;
 int main() {
     Game g;
     g.SetupGame("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8");
 
-    for(int i = 1; i<10; i++){
-    cout << g.SumOfAllMoves(i)<<endl;
-    }
+    clock_t tStart = clock();
+    cout << g.SumOfAllMoves(3)<<endl;
+    printf("Time taken: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+   
+
     
 }
