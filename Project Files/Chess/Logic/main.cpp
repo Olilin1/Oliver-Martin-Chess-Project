3047b1f92cf6e6e252456f4f38c7c7f8fb17ef8d
Oliver Lindgren <oliver.lindgren@outlook.com>
2021-04-28 17:49:10 +0200

    Testing func

--- Project Files/Chess/Logic/main.cpp
+++ Project Files/Chess/Logic/main.cpp
@@ -1,14 +1,28 @@
+#include<iostream>
 #include"Game.hpp"
-#include<time.h>
+#include"Tests.hpp"
 
 using namespace std;
-int main() {
-    Game g;
-    g.SetupGame("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -");
-    for(int i = 1; i <= 10; i++){
-    clock_t tStart = clock();
-    cout << g.SumOfAllMoves(i)<<endl;
-    printf("Time taken: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+
+void runTests(int depth){
+
+    for(test t : tests){
+        Game g;
+        g.SetupGame(t.fen);
+        cout << "Running test: " << t.testName << endl;
+        for(int i = 0; i <= depth; i++){
+            int ans = g.SumOfAllMoves(i);
+            if(ans == t.expected[i]){
+                cout << t.testName << " depth " << i << " PASS"<<endl; 
+            }
+            else{
+                cout << t.testName << " depth " << i << " FAIL"<< "expected " << t.expected[i] << " but got "<<ans <<endl; 
+            }
+        }
+        cout << endl;
     }
-    
+}
+
+int main() {
+    runTests(4);
 }
