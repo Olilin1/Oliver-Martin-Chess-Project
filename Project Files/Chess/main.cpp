9544a1d7dd1a407e353d2d33dcd2552cfd4e51e9
Oliver Lindgren <oliver.lindgren@outlook.com>
2021-05-12 23:19:13 +0200

    Added multiple modes

--- Project Files/Chess/main.cpp
+++ Project Files/Chess/main.cpp
@@ -1,11 +1,30 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include<assert.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+
+    bool ok;
+    QStringList items;
+    items << "PVP" << "DEBUG" << "PVEWHITE" << "PVEBLACK";
+    QString item = QInputDialog::getItem(NULL, "Launch mode",
+                                             "Mode:", items, 0, false, &ok);
+    LaunchMode mode;
+    if(ok){
+        if(item == "PVP") mode = PVP;
+        else if (item == "DEBUG") mode = DEBUG;
+        else if (item == "PVEWHITE") mode = PVEWHITE;
+        else if (item == "PVEBLACK") mode = PVEBLACK;
+        else assert(false);
+    }
+    else{
+        return 0;
+    }
+
+    MainWindow w(mode);
     w.show();
     return a.exec();
 }
