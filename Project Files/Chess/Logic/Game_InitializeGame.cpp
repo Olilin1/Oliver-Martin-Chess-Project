81b6b56c92d05d393209d3bfcdda8c9cdefb5b56
Martin Orrje <morrje@kth.se>
2021-05-18 02:52:15 +0200

    Commented out one line that should have been commented

--- Project Files/Chess/Logic/Game_InitializeGame.cpp
+++ Project Files/Chess/Logic/Game_InitializeGame.cpp
@@ -4,6 +4,6 @@ Game::Game(){
     AssignMagicNumbers();
     GenerateKingAttackBoards();
     GenerateKnightAttackBoards();
-    SetupGame("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - ");
+    //SetupGame("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     PrintBoard();
 }
