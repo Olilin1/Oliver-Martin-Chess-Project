edfa247db260e660d331dcf6315532aa82d4cfff
Oliver Lindgren <oliver.lindgren@outlook.com>
2021-09-24 20:18:57 +0200

    Added the time header to the test hpp

--- Project Files/Chess/Logic/Tests.hpp
+++ Project Files/Chess/Logic/Tests.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include<string>
 #include<vector>
+#include"time.h"
 
 struct test{
 std::string testName;
--- Project Files/Chess/Logic/main.cpp
+++ Project Files/Chess/Logic/main.cpp
@@ -5,12 +5,20 @@
 #include<iostream>
 #include <sstream>      // std::stringstream
 #include <ctime>
+#include<thread>
 
 /*Copy line below to run in terminal (at least on mac you have to do this...)
 g++ main.cpp Bitboard.cpp Game\_GenerateBlockerMoves.cpp Game\_MagicNumberGeneration.cpp Game\_KingKnightMoves.cpp Game\_GeneratePseudoMoves.cpp Game\_InitializeGame.cpp Game\_HelperFunctions.cpp Game\_MakeLegalMove.cpp Game\_MakeAllMoves.cpp Game\_GenerateAttackSets.cpp Game\_Minimax.cpp Game\_EvaluatePosition.cpp Game\_IsOver.cpp -std=c++17
 or "g++ *.cpp" should also work
 */
 
+using namespace std;
 int main() {
+    Game game();
+    string line;
+    while(getline(std::cin, line)){
+        stringstream ss(line);
+        string command;
 
+    }
 }
